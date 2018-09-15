d559eaa8cfbcd5e0ed248472324fbc42d59f8654
yichiang <yi_chiang@Outlook.com>
2018-09-15 10:49:55 -0700

    using wizard to create an Android Library

---
+++ jokeandroidview/src/main/java/com/example/jokeandroidview/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.jokeandroidview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/*
+Step 2: Create an Android Library
+Create an Android Library containing an Activity that will display a joke passed to it as an intent extra. Wire up project dependencies so that the button can now pass the joke from the Java Library to the Android Library.
+
+For review on how to create an Android library, check out demo 4.03. For a refresher on intent extras, check out;
+
+http://developer.android.com/guide/components/intents-filters.html
+
+
+* */
+public class MainActivity extends AppCompatActivity {
+
+    /*
+    example - https://developer.android.com/guide/components/intents-filters
+    static final String ACTION_TIMETRAVEL = "com.example.action.TIMETRAVEL";
+     */
+    static final String EXTRA_JOKE = "JOKE_INTENT_VALUE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void displayJoke()
+    {
+        TextView jokeTextView = (TextView) findViewById(R.id.joke_text_view);
+
+        //get intent -> Set text from extra
+        if(getIntent().hasExtra(EXTRA_JOKE))
+        {
+            jokeTextView.setText(getIntent().getStringExtra(EXTRA_JOKE));
+        }
+    }
+}
